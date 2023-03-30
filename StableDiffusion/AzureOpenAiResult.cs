ad8f007ff5fa55f9669b4f2f9360a2b8433b103c
brev <46505951+cassiebreviu@users.noreply.github.com>
2023-03-30 17:15:19 -0500

    Simple proof of concept test to switch Azure OAI Dall-E cloud and local Stable Diff renders

---
+++ StableDiffusion/AzureOpenAiResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StableDiffusion
+{
+    public class AzureOpenAiResult
+    {
+        public string id { get; set; }
+        public Result result { get; set; }
+        public string status { get; set; }
+    }
+
+
+    public class Result
+    {
+        public string caption { get; set; }
+        public string contentUrl { get; set; }
+        public string contentUrlExpiresAt { get; set; }
+        public string createdDateTime { get; set; }
+    }
+}
--- StableDiffusion/Program.cs
+++ StableDiffusion/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.ML.OnnxRuntime.Tensors;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
 
 namespace StableDiffusion
 {
@@ -6,54 +8,92 @@ namespace StableDiffusion
     {
         static void Main(string[] args)
         {
-            //test how long this takes to execute
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-            Directory.SetCurrentDirectory(@"..\..\..\..");
+            // Inference local or on Azure OAI Dall-E
+            var isLocal = true;
 
             //Default args
             var prompt = "a fireplace in an old cabin in the woods";
             Console.WriteLine(prompt);
 
-            // Number of denoising steps
-            var num_inference_steps = 15;
-            // Scale for classifier-free guidance
-            var guidance_scale = 7.5;
-            //num of images requested
-            var batch_size = 1;
-
-            // Load the tokenizer and text encoder to tokenize and encode the text.
-            var textTokenized = TextProcessing.TokenizeText(prompt);
-            var textPromptEmbeddings = TextProcessing.TextEncoder(textTokenized).ToArray();
-
-            // Create uncond_input of blank tokens
-            var uncondInputTokens = TextProcessing.CreateUncondInput();
-            var uncondEmbedding = TextProcessing.TextEncoder(uncondInputTokens).ToArray();
-
-            // Concant textEmeddings and uncondEmbedding
-            DenseTensor<float> textEmbeddings = new DenseTensor<float>(new[] { 2, 77, 768 });
-
-            for (var i = 0; i < textPromptEmbeddings.Length; i++)
+            if (isLocal)
             {
-                textEmbeddings[0, i / 768, i % 768] = uncondEmbedding[i];
-                textEmbeddings[1, i / 768, i % 768] = textPromptEmbeddings[i];
+
+                //test how long this takes to execute
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                Directory.SetCurrentDirectory(@"..\..\..\..");
+
+
+
+                // Number of denoising steps
+                var num_inference_steps = 15;
+                // Scale for classifier-free guidance
+                var guidance_scale = 7.5;
+                //num of images requested
+                var batch_size = 1;
+
+                // Load the tokenizer and text encoder to tokenize and encode the text.
+                var textTokenized = TextProcessing.TokenizeText(prompt);
+                var textPromptEmbeddings = TextProcessing.TextEncoder(textTokenized).ToArray();
+
+                // Create uncond_input of blank tokens
+                var uncondInputTokens = TextProcessing.CreateUncondInput();
+                var uncondEmbedding = TextProcessing.TextEncoder(uncondInputTokens).ToArray();
+
+                // Concant textEmeddings and uncondEmbedding
+                DenseTensor<float> textEmbeddings = new DenseTensor<float>(new[] { 2, 77, 768 });
+
+                for (var i = 0; i < textPromptEmbeddings.Length; i++)
+                {
+                    textEmbeddings[0, i / 768, i % 768] = uncondEmbedding[i];
+                    textEmbeddings[1, i / 768, i % 768] = textPromptEmbeddings[i];
+                }
+
+                var height = 512;
+                var width = 512;
+
+                // Inference Stable Diff
+                var image = UNet.Inference(num_inference_steps, textEmbeddings, guidance_scale, batch_size, height, width);
+
+                // If image failed or was unsafe it will return null.
+                if (image == null)
+                {
+                    Console.WriteLine("Unable to create image, please try again.");
+                }
+
+                // Stop the timer
+                watch.Stop();
+                var elapsedMs = watch.ElapsedMilliseconds;
+                Console.WriteLine("Time taken: " + elapsedMs + "ms");
             }
+            else
+            {   
+                // Update the url and api key from the Azure Open AI Resource. This is not Prod ready and is meant to be used as a proof of concept.
+                string url = "";
+                string api_key = "";
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Add("api-key", api_key);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var body = new
+                {
+                    caption = prompt,
+                    resolution = "1024x1024"
+                };
+                var response = client.PostAsJsonAsync(url, body).Result;
+                var operation_location = response.Headers.GetValues("Operation-Location").FirstOrDefault();
+                var retry_after = response.Headers.GetValues("Retry-after").FirstOrDefault();
+                var azureOpenAiResult = new AzureOpenAiResult();
+                azureOpenAiResult.status = "";
+                while (azureOpenAiResult.status != "Succeeded")
+                {
+                    System.Threading.Thread.Sleep(int.Parse(retry_after) * 1000);
+                    response = client.GetAsync(operation_location).Result;
+                    azureOpenAiResult = response.Content.ReadFromJsonAsync<AzureOpenAiResult>().Result;
+                }
+                // This is a link to the image that was created. Click the link to view the image.
+                Console.WriteLine(azureOpenAiResult.result.contentUrl);
 
-            var height = 512;
-            var width = 512;
-
-            // Inference Stable Diff
-            var image = UNet.Inference(num_inference_steps, textEmbeddings, guidance_scale, batch_size, height, width);
-
-            // If image failed or was unsafe it will return null.
-            if( image == null )
-            {
-                Console.WriteLine("Unable to create image, please try again.");
             }
-
-            // Stop the timer
-            watch.Stop();
-            var elapsedMs = watch.ElapsedMilliseconds;
-            Console.WriteLine("Time taken: " + elapsedMs + "ms");
 
         }
 
